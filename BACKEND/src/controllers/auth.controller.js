07e67ccfbec24ad9865612eee6c3f4997a0dc8bb
Shreyansh Upadhye <shreyanshu0056@gmail.com>
2025-09-09 10:45:45 +0530

    Implemented the user logout backend

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -70,7 +70,15 @@ async function loginUser(req, res) {
     })
  }
 
+function logoutUser(req, res) {
+    res.clearCookie('token');
+    res.status(200).json({
+        message: 'User Logged Out Successfully'
+    })
+}
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
