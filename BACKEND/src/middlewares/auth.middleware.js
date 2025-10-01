ba724c9031f7391d976f180f99696860b7498059
Shreyansh Upadhye <shreyanshu0056@gmail.com>
2025-10-01 20:16:23 +0530

    Implemented the video uploder using ImageKit cloude provider and multer

--- BACKEND/src/middlewares/auth.middleware.js
+++ BACKEND/src/middlewares/auth.middleware.js
@@ -2,31 +2,32 @@ const foodPartnerModel = require('../models/foodPartner.model');
 const jwt = require('jsonwebtoken');
 
 async function authFoodPartnerMiddleware(req, res, next) {
+
     const token = req.cookies.token;
 
     if (!token) {
-        return res.status(400).json({
+        return res.status(401).json({
             message: "Please login first"
         })
     }
 
     try {
-        
-        const decoded = jwt.verify(token, params.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         const foodPartner = await foodPartnerModel.findById(decoded.id);
 
-        req.foodPartner = foodPartner;
-        
-        next();
+        req.foodPartner = foodPartner
 
-    } catch (error) {
-        
+        next()
+
+    } catch (err) {
+
         return res.status(401).json({
             message: "Invalid token"
-        });
+        })
 
     }
+
 }
 
 module.exports = {
